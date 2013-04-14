d2cd863836365e548d810ae67a6ea462e3067bd6
James Marshall <jcm102120@gmail.com>
2013-04-14 17:37:23 -0500

    people update

---
+++ HP-TechCon Android/assets/www/js/People/people.js
@@ -0,0 +1,76 @@
+var userID;
+var _personID;
+
+var peopleTemplate;
+var peopleTemplateCompiled;
+
+$(function() {
+
+	var PARSE_APP = "9AeVfYuAP1SWUgUv5bogPOaGwldaZTstNEO8tdJx";
+	var PARSE_JS = "w4ffwNOQtdfqDb2tWBXUoPmD7qJrpmHv6xcnuZj4";
+
+	Parse.initialize(PARSE_APP, PARSE_JS);
+	
+	userID = Parse.User.current().getUsername();
+	
+	peopleTemplate = $("#people-template").html();
+    peopleTemplateCompiled = Handlebars.compile(peopleTemplate);
+	
+	$("#peoplediv").on('click', '#peopleList li', function(e) {
+		e.preventDefault();
+		_id = e.target.id;
+		
+		$.mobile.changePage('#person-details');
+	}); 
+
+	getPeople();
+
+});
+
+function getPeople(){
+	var name;
+	var lastName;
+	var img;
+	
+	var people = Parse.Object.extend("employee");
+
+	var query = new Parse.Query(people);
+	query.ascending("Last_Name");
+	
+	query.find({
+		success: function(result) {
+			
+			var data = '{ "peopleList" : [';
+			for(var i=0; i < result.length ;i++){
+				name = result[i].get("name");
+				lastName = result[i].get("Last_Name");
+				img = result[i].get("picture");
+				_personID = result[i].get("employee_id");
+				
+				data += '{"name": "' + name + '", ';
+				data += '"id": "' + _personID + '", ';
+				data += '"lastName": "' + lastName + '", ';
+				
+				if (i == result.length - 1){
+					data += '"image": "' + img + '"} ';				
+				}
+				else {
+				data += '"image": "' + img + '"}, ';
+				}
+			}
+			data += ']}';
+			
+			var arr = JSON.parse(data);		
+			
+			$("#peoplediv").html(peopleTemplateCompiled(arr)).trigger('create');
+			$("#peoplediv ul").listview('refresh');			
+			
+		},
+		error: function(result, error) {
+			// error is an instance of Parse.Error.
+			alert("Error");
+		}
+	
+	});
+				
+}
